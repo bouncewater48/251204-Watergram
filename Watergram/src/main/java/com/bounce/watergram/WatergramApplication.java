eca20f1d5d94ce22c1c6f1b0610dd3d010da210e
bouncewater <bouncewater48@gmail.com>
2025-12-04 16:01:57 +0900

    [강승훈] - Watergram 생성 - HelloWorld 확인

---
+++ Watergram/src/main/java/com/bounce/watergram/WatergramApplication.java
@@ -0,0 +1,13 @@
+package com.bounce.watergram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatergramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatergramApplication.class, args);
+    }
+
+}
---
+++ Watergram/src/test/java/com/bounce/watergram/WatergramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bounce.watergram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatergramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
