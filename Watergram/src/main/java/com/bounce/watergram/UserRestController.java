ffaa7c2bf8b8f5e76fad0e938ad35c9026962455
bouncewater <bouncewater48@gmail.com>
2025-12-08 16:37:08 +0900

    [강승훈] - Watergram 회원가입과 중복확인 유효성 검사 추가

--- Watergram/src/main/java/com/bounce/watergram/UserRestController.java
+++ Watergram/src/main/java/com/bounce/watergram/UserRestController.java
@@ -2,6 +2,7 @@ package com.bounce.watergram;
 
 import com.bounce.watergram.service.UserService;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import java.util.Map;
 @RestController
 public class UserRestController {
 
+    @Autowired
     private UserService userService;
 
     public Map<String, String> join(
